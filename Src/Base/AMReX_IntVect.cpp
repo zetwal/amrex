993c6225ef000c82148f575ec93533db6f8987fc
Ann Almgren <asalmgren@lbl.gov>
2018-03-08 15:41:29 -0800

    Modify the IntVect routines -- still need Weiqun to fix the shift defs

--- Src/Base/AMReX_IntVect.cpp
+++ Src/Base/AMReX_IntVect.cpp
@@ -10,21 +10,19 @@
 #include <AMReX_IndexType.H>
 #include <AMReX_Utility.H>
 
-
 namespace amrex {
-
 
 const IntVect&
 IntVect::TheUnitVector ()
 {
-    static const IntVect Unit(AMREX_D_DECL(1,1,1));
+    static const IntVect Unit(AMREX_D_DECL6(1,1,1,1,1,1));
     return Unit;
 }
 
 const IntVect&
 IntVect::TheZeroVector ()
 {
-    static const IntVect Zero(AMREX_D_DECL(0,0,0));
+    static const IntVect Zero(AMREX_D_DECL6(0,0,0,0,0,0));
     return Zero;
 }
 
@@ -34,10 +32,10 @@ IntVect::TheZeroVector ()
 int IntVect::InitStatics()
 {
   IntVect* pz = const_cast<IntVect*>( &IntVect::Zero );
-  *pz = IntVect(AMREX_D_DECL(0,0,0));
+  *pz = IntVect(AMREX_D_DECL6(0,0,0,0,0,0));
 
   IntVect* pu = const_cast<IntVect*>( &IntVect::Unit );
-  *pu = IntVect(AMREX_D_DECL(1,1,1));
+  *pu = IntVect(AMREX_D_DECL6(1,1,1,1,1,1));
 
   // No danger of IntVect::Zero and Unit not having been allocated, as ARM section
   // 3.4 says "The initialization of nonlocal static objects in a translation unit
@@ -60,18 +58,33 @@ IntVect::TheDimensionVector (int d)
     switch (d) {
     case (0) :
     {
-	static const IntVect xdim(AMREX_D_DECL(1,0,0));
+	static const IntVect xdim(AMREX_D_DECL6(1,0,0,0,0,0));
 	return xdim;
     }
     case (1) :
     {
-	static const IntVect ydim(AMREX_D_DECL(0,1,0));
+	static const IntVect ydim(AMREX_D_DECL6(0,1,0,0,0,0));
 	return ydim;
+    }
+    case (2) :
+    {
+	static const IntVect ydim(AMREX_D_DECL6(0,0,1,0,0,0));
+	return ydim;
+    }
+    case (3) :
+    {
+	static const IntVect udim(AMREX_D_DECL6(0,0,0,1,0,0));
+	return udim;
+    }
+    case (4) :
+    {
+	static const IntVect vdim(AMREX_D_DECL6(0,0,0,0,1,0));
+	return vdim;
     }
     default:
     {
-	static const IntVect zdim(AMREX_D_DECL(0,0,1));
-	return zdim;
+	static const IntVect wdim(AMREX_D_DECL6(0,0,0,0,0,1));
+	return wdim;
     }
     };
 }
@@ -79,44 +92,54 @@ IntVect::TheDimensionVector (int d)
 const IntVect&
 IntVect::TheNodeVector ()
 {
-    static const IntVect Node(AMREX_D_DECL(IndexType::NODE,IndexType::NODE,IndexType::NODE));
+    static const IntVect Node(AMREX_D_DECL6(IndexType::NODE,IndexType::NODE,IndexType::NODE,
+                                            IndexType::NODE,IndexType::NODE,IndexType::NODE));
     return Node;
 }
 
 const IntVect&
 IntVect::TheCellVector ()
 {
-    static const IntVect Cell(AMREX_D_DECL(IndexType::CELL,IndexType::CELL,IndexType::CELL));
+    static const IntVect Cell(AMREX_D_DECL6(IndexType::CELL,IndexType::CELL,IndexType::CELL,
+                                            IndexType::CELL,IndexType::CELL,IndexType::CELL));
     return Cell;
 }
 
 const IntVect&
 IntVect::TheMaxVector ()
 {
-    static const IntVect mx(AMREX_D_DECL(std::numeric_limits<int>::max(),
-                                   std::numeric_limits<int>::max(),
-                                   std::numeric_limits<int>::max()));
+    static const IntVect mx(AMREX_D_DECL6(std::numeric_limits<int>::max(),
+                                          std::numeric_limits<int>::max(),
+                                          std::numeric_limits<int>::max(),
+                                          std::numeric_limits<int>::max(),
+                                          std::numeric_limits<int>::max(),
+                                          std::numeric_limits<int>::max()));
     return mx;
 }
 
 const IntVect&
 IntVect::TheMinVector ()
 {
-    static const IntVect mn(AMREX_D_DECL(std::numeric_limits<int>::min(),
-                                   std::numeric_limits<int>::min(),
-                                   std::numeric_limits<int>::min()));
+    static const IntVect mn(AMREX_D_DECL6(std::numeric_limits<int>::min(),
+                                          std::numeric_limits<int>::min(),
+                                          std::numeric_limits<int>::min(),
+                                          std::numeric_limits<int>::min(),
+                                          std::numeric_limits<int>::min(),
+                                          std::numeric_limits<int>::min()));
     return mn;
 }
 
 IntVect::IntVect (const int *a)
 {
-    AMREX_D_EXPR(vect[0] = a[0], vect[1] = a[1], vect[2] = a[2]);
+    AMREX_D_EXPR6(vect[0] = a[0], vect[1] = a[1], vect[2] = a[2], 
+                  vect[3] = a[3], vect[4] = a[4], vect[5] = a[5]);
 }
 
 IntVect::IntVect (const Vector<int> &a)
 {
     BL_ASSERT(a.size() == AMREX_SPACEDIM);
-    AMREX_D_EXPR(vect[0] = a[0], vect[1] = a[1], vect[2] = a[2]);
+    AMREX_D_EXPR6(vect[0] = a[0], vect[1] = a[1], vect[2] = a[2], 
+                  vect[3] = a[3], vect[4] = a[4], vect[5] = a[5]);
 }
 
 IntVect
@@ -149,7 +172,7 @@ BASISV (int dir)
 IntVect
 scale (const IntVect& p, int s)
 {
-    return IntVect(AMREX_D_DECL(s * p[0], s * p[1], s * p[2]));
+    return IntVect(AMREX_D_DECL6(s * p[0], s * p[1], s * p[2], s * p[3], s * p[4], s * p[5]));
 }
 
 IntVect
@@ -166,7 +189,7 @@ reflect (const IntVect& a,
 IntVect
 diagShift (const IntVect& p, int s)
 {
-    return IntVect(AMREX_D_DECL(p[0] + s, p[1] + s, p[2] + s));
+    return IntVect(AMREX_D_DECL6(p[0] + s, p[1] + s, p[2] + s, p[3] + s, p[4] + s, p[5] + s));
 }
 
 IntVect
@@ -175,7 +198,7 @@ coarsen (const IntVect& p,
 {
     BL_ASSERT(s > 0);
     IntVect v = p;
-    v.coarsen(IntVect(AMREX_D_DECL(s,s,s)));
+    v.coarsen(IntVect(AMREX_D_DECL6(s,s,s,s,s,s)));
     return v;
 }
 
@@ -192,7 +215,7 @@ IntVect&
 IntVect::coarsen (int s)
 {
     BL_ASSERT(s > 0);
-    return this->coarsen(IntVect(AMREX_D_DECL(s,s,s)));
+    return this->coarsen(IntVect(AMREX_D_DECL6(s,s,s,s,s,s)));
 }
 
 IntVect&
@@ -219,8 +242,11 @@ operator<< (std::ostream&  os,
             const IntVect& p)
 {
     os << AMREX_D_TERM( '(' << p[0] , <<
-                  ',' << p[1] , <<
-                  ',' << p[2])  << ')';
+                        ',' << p[1] , <<
+                        ',' << p[2] , <<
+                        ',' << p[3] , <<
+                        ',' << p[4] , <<
+                        ',' << p[5])  << ')';
     if (os.fail())
         amrex::Error("operator<<(ostream&,IntVect&) failed");
     return os;
@@ -241,9 +267,12 @@ operator>> (std::istream& is,
 
     if (c == '(')
     {
-        AMREX_D_EXPR(is >> iv[0],
+        AMREX_D_EXPR6(is >> iv[0],
                is.ignore(BL_IGNORE_MAX, ',') >> iv[1],
-               is.ignore(BL_IGNORE_MAX, ',') >> iv[2]);
+               is.ignore(BL_IGNORE_MAX, ',') >> iv[2],
+               is.ignore(BL_IGNORE_MAX, ',') >> iv[3],
+               is.ignore(BL_IGNORE_MAX, ',') >> iv[4],
+               is.ignore(BL_IGNORE_MAX, ',') >> iv[5]);
         is.ignore(BL_IGNORE_MAX, ')');
     }
     else
